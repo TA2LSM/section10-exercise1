c0b3b829c4b44c9361ec8b51224c195ce135bcde
TA2LSM <semihsenol@gmail.com>
2022-03-21 16:32:04 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const Joi = require("joi");
+Joi.objectId = require("joi-objectid")(Joi);
+// Bu modül, Joi modülüne yeni bir metot (fonksiyon) ekliyor aslında.
+
+const genres = require("./routes/genres");
+const customers = require("./routes/customers");
+const movies = require("./routes/movies");
+const rentals = require("./routes/rentals");
+const users = require("./routes/users");
+
+const express = require("express");
+const app = express();
+
+mongoose
+  .connect("mongodb://localhost/vidly")
+  .then(() => {
+    console.log("Connected to the database...");
+  })
+  .catch((err) => {
+    console.error("Couldn't connect to the database!");
+  });
+
+app.use(express.json());
+app.use("/api/genres", genres);
+app.use("/api/customers", customers);
+app.use("/api/movies", movies);
+app.use("/api/rentals", rentals);
+app.use("/api/users", users);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
